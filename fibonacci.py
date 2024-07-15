646afa2d0e9f8830d6ccaffd5f1e8d12ebeb963e
Rufina Aristyani <61680841+RufinaAristyani080394@users.noreply.github.com>
2024-07-15 16:13:58 +0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,1 @@
-from fibonacci import generate_fibonacci
 
-def main():
-    try:
-        limit = int(input("Enter the number of Fibonacci terms: "))
-        fibonacci_numbers = fibonacci(limit)
-        
-        print(f"Fibonacci numbers of length {limit}:")
-        print(', '.join(map(str, fibonacci_sequence)))
-        
-    except Exception:
-        print("wrong input!")
-    
-if __name__ == "__main__":
-    main()
